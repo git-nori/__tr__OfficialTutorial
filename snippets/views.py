0d4ce96f4efca394aa7800334df98b635a8febff
HironoriSato <maza.hs5360@gmail.com>
2019-12-31 16:19:20 +0900

    [snippet]add:viewに対して認証機能の追加, BrowsableAPIへのログイン機能の追加

--- snippets/views.py
+++ snippets/views.py
@@ -3,11 +3,13 @@ from .models import Snippet
 from .serializers import SnippetSerializer, UserSerializer
 from rest_framework import generics
 from django.contrib.auth.models import User
+from rest_framework import permissions
 
 
 class SnippetList(generics.ListAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     # serializerのcreateメソッドに'owner'が渡される => snippetの作成時にUser(外部キー)を関連付ける
     def perform_create(self, serializer):
@@ -17,6 +19,7 @@ class SnippetList(generics.ListAPIView):
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 
 class UserList(generics.ListAPIView):
