d5076c8413d96d0dcec5cd8cbc3d9c2e4c6190db
HironoriSato <maza.hs5360@gmail.com>
2019-12-31 15:43:57 +0900

    [snippets]mod:ジェネリッククラスベースビューを使用してviewを修正

--- snippets/views.py
+++ snippets/views.py
@@ -2,34 +2,13 @@ from django.shortcuts import render
 from .models import Snippet
 from .serializers import SnippetSerializer
 from rest_framework import generics
-from rest_framework import mixins
 
 
-class SnippetList(mixins.ListModelMixin,
-                  mixins.CreateModelMixin,
-                  generics.GenericAPIView):
+class SnippetList(generics.ListAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(self, request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        return self.create(self, request, *args, **kwargs)
-
-
-class SnippetDetail(mixins.RetrieveModelMixin,
-                    mixins.UpdateModelMixin,
-                    mixins.DestroyModelMixin,
-                    generics.GenericAPIView):
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(self, request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(self, request, *args, **kwargs)
-
-    def destroy(self, request, *args, **kwargs):
-        return self.destroy(self, request, *args, **kwargs)
