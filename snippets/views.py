820d0b249775ba9f3987e09bf9ef51a8cd87d3dd
HironoriSato <maza.hs5360@gmail.com>
2019-12-31 16:12:52 +0900

    [snippet]add:Userモデルに対応したviewの作成

--- snippets/views.py
+++ snippets/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from .models import Snippet
-from .serializers import SnippetSerializer
+from .serializers import SnippetSerializer, UserSerializer
 from rest_framework import generics
+from django.contrib.auth.models import User
 
 
 class SnippetList(generics.ListAPIView):
@@ -12,3 +13,13 @@ class SnippetList(generics.ListAPIView):
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
