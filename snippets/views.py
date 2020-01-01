32a23d7b218e06fff5a869b099b6343df2692fbb
HironoriSato <maza.hs5360@gmail.com>
2020-01-01 11:46:18 +0900

    [snippet]mod:routersを使用してurlsをリファクタリング

--- snippets/views.py
+++ snippets/views.py
@@ -1,23 +1,13 @@
 from django.shortcuts import render
 from .models import Snippet
 from .serializers import SnippetSerializer, UserSerializer
-from rest_framework import generics, renderers
+from rest_framework import renderers
 from django.contrib.auth.models import User
 from rest_framework import permissions
 from .permissions import IsOwnerOrReadOnly
-from rest_framework.decorators import api_view, action
+from rest_framework.decorators import action
 from rest_framework.response import Response
-from rest_framework.reverse import reverse
 from rest_framework import viewsets
-
-
-# APIのルートのエンドポイント
-@api_view(['GET'])
-def api_root(request, format=None):
-    return Response({
-        'users': reverse('user-list', request=request, format=format),
-        'snippets': reverse('snippet-list', request=request, format=format),
-    })
 
 
 class SnippetViewSet(viewsets.ModelViewSet):
