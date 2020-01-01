388caec386393116fb4c23b96d2861eefaba44fc
HironoriSato <maza.hs5360@gmail.com>
2020-01-01 11:38:40 +0900

    [snippet]mod:汎用クラスビューをviewsetに変更

--- snippets/views.py
+++ snippets/views.py
@@ -5,9 +5,10 @@ from rest_framework import generics, renderers
 from django.contrib.auth.models import User
 from rest_framework import permissions
 from .permissions import IsOwnerOrReadOnly
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, action
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
+from rest_framework import viewsets
 
 
 # APIのルートのエンドポイント
@@ -19,41 +20,27 @@ def api_root(request, format=None):
     })
 
 
-# snippetコードをHTMLにレンダリングするため設定(tutorialに依存)
-class SnippetHighlight(generics.GenericAPIView):
-    queryset = Snippet.objects.all()
-    renderer_classes = [renderers.StaticHTMLRenderer]
-
-    def get(self, request, *args, **kwargs):
-        snippet = self.get_object()
-        return Response(snippet.highlighted)
-
-
-class SnippetList(generics.ListAPIView):
+class SnippetViewSet(viewsets.ModelViewSet):
+    """list, create, retrieve, update, destroyアクションを持つ"""
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly]
+
+    # actionデコレータを使用して標準のcreate, update, deleteスタイルに適合しないカスタムエンドポイントを定義
+    # snippetコードをHTMLにレンダリングするため設定(tutorialに依存)
+    @action(detail=True, renderer_classes=[renderers.StaticHTMLRenderer])
+    def highlight(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
 
     # serializerのcreateメソッドに'owner'が渡される => snippetの作成時にUser(外部キー)を関連付ける
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
 
-class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Snippet.objects.all()
-    serializer_class = SnippetSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
-                          IsOwnerOrReadOnly]
-
-
-class UserList(generics.ListAPIView):
+# ReadOnlyModelViewSet => 読み取り専用
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    """list, detailアクションを持つ"""
     queryset = User.objects.all()
     serializer_class = UserSerializer
-
-
-class UserDetail(generics.RetrieveAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-
-
